530610149e2cad011b5cde6530a1de31948739e6
kolaider <n.strom@atomgroup.io>
2025-04-13 15:17:36 +0300

    pass module 13

---
+++ section-02-basics/13-string-interpolation/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const category = 'toys';
+// console.log('https://someurl.com/' + category + '/' + 4) // не очень удобный способ
+
+console.log(`https://someurl.com/${category}/5`) // интерполяция
+
+
+const user =  'Ivan';
+
+alert(`Привет ${user}`);
