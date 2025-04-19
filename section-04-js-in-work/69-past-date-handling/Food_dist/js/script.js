f0414a94cb05fe39ad5082a530373a738c360021
kolaider <nikolai.strom@skz.by>
2025-04-19 12:54:34 +0300

    pass module 69

--- section-04-js-in-work/69-past-date-handling/Food_dist/js/script.js
+++ section-04-js-in-work/69-past-date-handling/Food_dist/js/script.js
@@ -44,14 +44,24 @@ window.addEventListener('DOMContentLoaded', () => {
 
     // Timer
 
-    const deadline = '2025-05-11';
+    const deadline = '2024-05-11';
 
     function getTimeRemaining(endTime) {
-        const t = Date.parse(endTime) - Date.parse(new Date()),
-              days = Math.floor(t / ( 1000 * 60 * 60 * 24 )),
-              hours = Math.floor((t / (1000 * 60 * 60) % 24)),
-              minutes = Math.floor(((t / 1000) / 60) % 60),
-              seconds = Math.floor((t / 1000) % 60);
+
+        let days, hours, minutes, seconds;
+        const t = Date.parse(endTime) - Date.parse(new Date());
+        
+        if (t <= 0) {
+            days = 0;
+            hours = 0;
+            minutes = 0;
+            seconds = 0;
+        } else {
+            days = Math.floor(t / ( 1000 * 60 * 60 * 24 )),
+            hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+            minutes = Math.floor(((t / 1000) / 60) % 60),
+            seconds = Math.floor((t / 1000) % 60);
+        }
         return {
             'total': t,
             'days': days,
@@ -63,7 +73,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     function getZero(num) {
         if (num >= 0 && num < 10) {
-            return `$0{num}`
+            return `0${num}`
         } else {
             return num;
         }
