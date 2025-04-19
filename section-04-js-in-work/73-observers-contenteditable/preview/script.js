1faa283c419ccd6721bcffc2079d75b0deececde
kolaider <nikolai.strom@skz.by>
2025-04-19 14:54:22 +0300

    pass module 73

--- section-04-js-in-work/73-observers-contenteditable/preview/script.js
+++ section-04-js-in-work/73-observers-contenteditable/preview/script.js
@@ -1,3 +1,17 @@
 'use strict';
 
 const box = document.querySelector('.box');
+
+let observer = new MutationObserver(mutationRecords => {
+    console.log(mutationRecords);
+});
+
+observer.observe(box, {
+    childList: true
+})
+// нельзя контролировать в момменте ввода текста, только после
+// операция асинхронная
+
+observer.disconnect()
+
+// другие observer
